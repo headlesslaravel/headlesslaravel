eb8d8aed74ff0b9fd45c0172187aecfdc2c079fa
Mitul Golakiya <mtl.golakiya@gmail.com>
2022-01-21 14:44:53 +0530

    feat: #2 Add vue assets during install

---
+++ src/Commands/InstallsVueStack.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace HeadlessLaravel\Commands;
+
+trait InstallsVueStack
+{
+    public function installVueStack()
+    {
+        // composer require laravel/breeze
+        $this->requireComposerPackages('laravel/breeze:^1.7');
+
+        // Install Breeze Vue Stack
+        $this->runBreezeInstall();
+
+        // NPM Packages...
+        $this->updateNodePackages(function ($packages) {
+            return [
+                    '@craniums/vue' => '^0.0.10',
+                ] + $packages;
+        });
+
+        $this->updateAppJs();
+        $this->updateTailwindConfig();
+        $this->runNPMInstall();
+        $this->runNPMProd();
+    }
+
+    protected function updateAppJs()
+    {
+        $addImportAfter = "import { InertiaProgress } from '@inertiajs/progress';";
+        $importCranium = "import Cranium from '@craniums/vue';";
+        $this->addLineAfter($addImportAfter, $importCranium, resource_path('js/app.js'));
+
+        $addUseAfter = ".use(plugin)";
+        $useCranium = "            .use(Cranium)";
+        $this->addLineAfter($addUseAfter, $useCranium, resource_path('js/app.js'));
+    }
+
+    protected function updateTailwindConfig()
+    {
+        $addTailwindContentAfter = "'./resources/js/**/*.vue',";
+        $tailwindContent = "        './node_modules/@craniums/vue/src/**/*.vue',";
+        $this->addLineAfter($addTailwindContentAfter, $tailwindContent, base_path('tailwind.config.js'));
+    }
+}
