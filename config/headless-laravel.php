3cb6cff24e38ddad89ffb42edf11024ae914cd13
Brian Dillingham <brian@dillingham.dev>
2022-01-26 10:33:07 -0500

    Tweak headless() (#5)

--- config/headless-laravel.php
+++ config/headless-laravel.php
@@ -2,8 +2,20 @@
 
 return [
 
-    'formations_path' => app_path('Http/Formations'),
+    /*
+    |--------------------------------------------------------------------------
+    | Headless class paths
+    |--------------------------------------------------------------------------
+    |
+    | When using Route::headless(), the following paths are used to determine
+    | the package paths for things like formations or cards to route them.
+    | You can change these paths if your application has a unique needs.
+    |
+    */
+    'paths' => [
 
-    'cards_path' => app_path('Http/Cards'),
+        'formations' => app_path('Http/Formations'),
 
+        'cards' => app_path('Http/Cards'),
+    ],
 ];
