bb26e32c1165741ed79ee01c32707c5e64819fa8
Brian Dillingham <dillingham@users.noreply.github.com>
2022-01-28 18:52:14 +0000

    Apply fixes from StyleCI

--- src/DateInterval.php
+++ src/DateInterval.php
@@ -13,58 +13,58 @@ class DateInterval
         return [
             'all' => [
                 'display' => 'All Time',
-                'from' => '',
-                'to' => ''
+                'from'    => '',
+                'to'      => '',
             ],
             'day' => [
                 'display' => 'Today',
-                'from' => $today,
-                'to' => $today->endOfDay()
+                'from'    => $today,
+                'to'      => $today->endOfDay(),
             ],
             'sub_day' => [
                 'display' => 'Yesterday',
-                'from' => $today->subDay(),
-                'to' => $today->subDay()->endOfDay()
+                'from'    => $today->subDay(),
+                'to'      => $today->subDay()->endOfDay(),
             ],
             'week' => [
                 'display' => 'This Week',
-                'from' => $today->startOfWeek(),
-                'to' => $today->endOfWeek()
+                'from'    => $today->startOfWeek(),
+                'to'      => $today->endOfWeek(),
             ],
             'sub_week' => [
                 'display' => 'Last Week',
-                'from' => $today->subWeek()->startOfWeek(),
-                'to' => $today->subWeek()->endOfWeek()
+                'from'    => $today->subWeek()->startOfWeek(),
+                'to'      => $today->subWeek()->endOfWeek(),
             ],
             'month' => [
                 'display' => 'This Month',
-                'from' => $today->startOfMonth(),
-                'to' => $today->endOfMonth()
+                'from'    => $today->startOfMonth(),
+                'to'      => $today->endOfMonth(),
             ],
             'sub_month' => [
                 'display' => 'Last Month',
-                'from' => $today->subMonth()->startOfMonth(),
-                'to' => $today->subMonth()->endOfMonth()
+                'from'    => $today->subMonth()->startOfMonth(),
+                'to'      => $today->subMonth()->endOfMonth(),
             ],
             'quarter' => [
                 'display' => 'This Quarter',
-                'from' => $today->startOfQuarter(),
-                'to' => $today->endOfQuarter()
+                'from'    => $today->startOfQuarter(),
+                'to'      => $today->endOfQuarter(),
             ],
             'sub_quarter' => [
                 'display' => 'Last Quarter',
-                'from' => $today->subQuarter()->startOfQuarter(),
-                'to' => $today->subQuarter()->endOfQuarter()
+                'from'    => $today->subQuarter()->startOfQuarter(),
+                'to'      => $today->subQuarter()->endOfQuarter(),
             ],
             'year' => [
                 'display' => 'This Year',
-                'from' => $today->startOfYear(),
-                'to' => $today->endOfYear()
+                'from'    => $today->startOfYear(),
+                'to'      => $today->endOfYear(),
             ],
             'sub_year' => [
                 'display' => 'Last Year',
-                'from' => $today->subYear()->startOfYear(),
-                'to' => $today->subYear()->endOfYear()
+                'from'    => $today->subYear()->startOfYear(),
+                'to'      => $today->subYear()->endOfYear(),
             ],
         ];
     }
